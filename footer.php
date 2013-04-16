a2b13bff037e6c09caafcb549c197d01954689cd
Dobronszki János <janosdobronszki@citromail.hu>
2013-04-16 10:54:48 +0200

    Init.

---
+++ footer.php
@@ -0,0 +1,11 @@
+    <div class="clear"></div>
+    
+    <div id="footer">
+        &copy; 2013 www.celebqa.com. All Rigts Reserved. Designed by <a href="http://www.dessign.net">Dessign.net</a>. Design inspired by <a href="http://www.fontfabric.com"> FontFabric.com</a>.
+    </div><!--//footer-->
+
+</div><!--//main_container-->
+
+<?php wp_footer(); ?>
+</body>
+</html>
